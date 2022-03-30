05a9688677eef99c2cd69335a0942ae00e5db39d
Siu <siuc@protonmail.com>
2022-03-30 19:30:03 -0400

    make gradient

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div v-if="error">{{ error }}</div>
+  <img class="cover" src="./assets/backgrounds/foggy.png" alt="" />
+  <div class="cover-gradient">
+    <div class="cover-gradient-top"></div>
+    <div class="cover-gradient-bottom"></div>
+  </div>
 </template>
 
 <script>
@@ -7,11 +11,41 @@ import getWeather from "./composables/getWeather";
 
 export default {
   setup() {
-    const { error } = getWeather();
+    const { error, location } = getWeather();
     return { error };
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.cover {
+  width: 100vw;
+  z-index: -1;
+}
+
+.cover-gradient {
+  position: absolute;
+  top: 20vh;
+  left: 0;
+  width: 100vw;
+}
+
+.cover-gradient-top {
+  background-image: linear-gradient(to bottom, #ffffff00, #ffffffff);
+  height: 45vh;
+}
+
+.cover-gradient-bottom {
+  height: 20vh;
+  background: #ffffff;
+}
 </style>
