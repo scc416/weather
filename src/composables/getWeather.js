c2703b7f44f4727c0dcd64a5db2302e6485a205a
Siu <siuc@protonmail.com>
2022-03-30 17:15:56 -0400

    get weather data

--- src/composables/getWeather.js
+++ src/composables/getWeather.js
@@ -1,10 +1,18 @@
+import axios from "axios";
+
 const getWeather = () => {
-  navigator.geolocation.getCurrentPosition((position) => {
+  navigator.geolocation.getCurrentPosition(async (position) => {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-    console.log(lat, long, timezone);
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,snow_depth,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=${timezone}`;
+    try {
+      const { data } = await axios.get(url);
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   });
 };
 
