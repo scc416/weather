470d5f9915299959e0a5358767ef91339e363b0a
Siu <siuc@protonmail.com>
2022-03-30 17:11:27 -0400

    get timezone

--- src/composables/getWeather.js
+++ src/composables/getWeather.js
@@ -2,8 +2,9 @@ const getWeather = () => {
   navigator.geolocation.getCurrentPosition((position) => {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-    console.log(lat, long);
+    console.log(lat, long, timezone);
   });
 };
 
