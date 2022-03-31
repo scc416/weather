48bcd656f038ba08fd5dc29bb177a1ab22e32e3a
Siu <siuc@protonmail.com>
2022-03-31 13:24:38 -0400

    move elm to wave

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <CoverImage :code="data.weatherCode" />
   <Rain v-if="true" />
-  <div class="cover-gradient">
-    <div class="wave"></div>
-  </div>
+  <Wave />
 
   <div class="content">
     {{ data.temperature }}
@@ -22,19 +20,15 @@
 <script>
 import getWeather from "./composables/getWeather";
 import Rain from "./components/cover/Rain.vue";
+import Wave from "./components/cover/Wave.vue";
 import CoverImage from "./components/cover/CoverImage.vue";
-import { watchEffect } from "vue";
 
 export default {
   setup() {
     const data = getWeather();
-
-    watchEffect(() => {
-      if ("data" in data) console.log(data.data.value);
-    });
     return data;
   },
-  components: { Rain, CoverImage },
+  components: { Rain, CoverImage, Wave },
 };
 </script>
 
@@ -56,15 +50,6 @@ body {
   box-sizing: border-box;
 }
 
-.cover-gradient {
-  position: relative;
-  width: 110vw;
-  top: 20vw;
-  left: -5vw;
-  z-index: 0;
-  color: #fff;
-}
-
 .content {
   position: relative;
   z-index: 1;
@@ -74,37 +59,5 @@ body {
   font-size: 0.6em;
   position: relative;
   bottom: 0.1em;
-}
-
-.wave:before {
-  border-bottom: 15vw solid #fff;
-  content: "";
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 30vw;
-  background-size: 60vw 120vw;
-  background-image: radial-gradient(
-    circle at 30vw -45vw,
-    transparent 60vw,
-    #fff 63vw
-  );
-}
-
-.wave:after {
-  border-bottom: 15vw solid #fff;
-  content: "";
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 45vw;
-  background-size: 120vw 120vw;
-  background-image: radial-gradient(
-    circle at 30vw 78vw,
-    #fff 60vw,
-    transparent 63vw
-  );
 }
 </style>
--- src/components/cover/Wave.vue
+++ src/components/cover/Wave.vue
@@ -1,13 +1,52 @@
 <template>
-  
+  <div class="cover-gradient">
+    <div class="wave"></div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.cover-gradient {
+  position: relative;
+  width: 110vw;
+  top: 20vw;
+  left: -5vw;
+  z-index: 0;
+  color: #fff;
+}
 
+.wave:before {
+  border-bottom: 15vw solid #fff;
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 30vw;
+  background-size: 60vw 120vw;
+  background-image: radial-gradient(
+    circle at 30vw -45vw,
+    transparent 60vw,
+    #fff 63vw
+  );
+}
+
+.wave:after {
+  border-bottom: 15vw solid #fff;
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 45vw;
+  background-size: 120vw 120vw;
+  background-image: radial-gradient(
+    circle at 30vw 78vw,
+    #fff 60vw,
+    transparent 63vw
+  );
+}
 </style>
